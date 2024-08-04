95b4b17b39d4417f999983c31a135677d6b35576
ganga <gangadhar.vasa99@gmail.com>
2024-08-04 23:59:09 +0530

    Adding a new HTTP Endpoint

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package example.com.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +9,11 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
         }
     }
 }
